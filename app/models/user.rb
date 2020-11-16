d98e6ea5013fb9d4a526e4fa46566ac9719321c1
Daniel Kurz <danieljkurz@gmail.com>
2020-11-16 16:38:33 +1000

    Added validation to sign-up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :listings, dependent: :destroy
   has_many :programming_languages, dependent: :destroy
+
+  validate :first_name_one_word, :last_name_one_word, :letters_only
+
+  def first_name_one_word
+    if first_name.include? " "
+      errors.add(:first_name, "cannot be more than 1 word")
+    end
+  end
+
+  def letters_only
+    if first_name !~ /\A[a-zA-Z]\z/
+      errors.add(:first_name, "Invalid characters, First Name can only contain letters")
+    end
+  end
+
+  def last_name_one_word
+    if last_name.include? " "
+      errors.add(:last_name, "cannot be more than 1 word")
+    end
+  end
 end
