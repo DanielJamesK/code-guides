5d57ab85a66539c33c91e9506acc18ba0e236e66
DanielJamesK <danieljkurz@gmail.com>
2020-11-22 03:49:50 +1000

    Added images to programming languages and listings dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,10 @@ ProgrammingLanguage.destroy_all
 ruby = ProgrammingLanguage.create!({user_id: users[0].id, name: "Ruby", description: "Ruby is an interpreted, high-level, general-purpose programming language. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming."})
 ruby.picture.attach(io: File.open(File.join(Rails.root, 'app/assets/images/RUBY.png')), filename: 'defaultimage1.png')
 ruby.save!
+
 javascript = ProgrammingLanguage.create!({user_id: users[0].id, name: "Javascript", description: "JavaScript is a scripting language that is one of the three core languages used to develop websites."})
+javascript.picture.attach(io: File.open(File.join(Rails.root, 'app/assets/images/Javascript.png')), filename: 'defaultimage1.png')
+javascript.save!
 
 Listing.destroy_all
 listing1 = Listing.create!({user_id: users[0].id, tutor_name: "Daniel K", tutor_email: "Danielseed@test.com", title: "Ruby Tutor", description: "I am an experineced tutor with more than 10 years experience", price: 25.00, programming_language_id: ruby.id})
